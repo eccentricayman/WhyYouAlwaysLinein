fdd192c74877d59b355f1660048fef43dd9a5ffd
eccentricayman <aahmed3@stuy.edu>
2017-02-11 16:19:34 -0500

    minah fix

--- draw.py
+++ draw.py
@@ -6,7 +6,7 @@ def draw_line( screen, x0, y0, x1, y1, color ):
     
     dx = 2 * (x1 - x0)
     dy = 2 * (y1 - y0)
-    d = dy + dx
+    d = dy + (x1 - x0)
 
     while x < x1:
         plot(screen, color, x, y)
