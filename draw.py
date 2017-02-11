661eaf3bee28a8cb26e83b9ca54dff843c8ff9eb
eccentricayman <aahmed3@stuy.edu>
2017-02-11 16:14:11 -0500

    gitignore and it just gives me black screen o.O

--- draw.py
+++ draw.py
@@ -1,4 +1,17 @@
 from display import *
 
 def draw_line( screen, x0, y0, x1, y1, color ):
-    pass
+    x = x0
+    y = y0
+    
+    dx = 2 * (x1 - x0)
+    dy = 2 * (y1 - y0)
+    d = dy + dx
+
+    while x < x1:
+        plot(screen, color, x, y)
+        if (d > 0):
+            y += 1
+            d += dx
+        x += 1
+        d += dy
