54e1bb97397c9b90bb6d55c264a42ed884d55092
eccentricayman <aahmed3@stuy.edu>
2017-02-16 02:04:00 -0500

    WHEW DONE

--- draw.py
+++ draw.py
@@ -3,65 +3,68 @@ from display import *
 def draw_line( x0, y0, x1, y1, screen, color ):
     x = x0
     y = y0
-    
-    dx = x1 - x0
+
+    dx = x0 - x1
     dy = y1 - y0
-    #d = dy + (x1 - x0)
 
-    if x0 > x1:
+    #deal with quadrant two and quadrant three
+    if (x0 > x1):
         draw_line(x1, y1, x0, y0, screen, color)
-
-    #quadrant 1
-    if (dy >= 0):
-        #octant 1
+        return 0
+    
+    #first quadrant
+    if (dy > 0):
+        #first octant
         if (-dx >= dy):
+            d = (2 * dy) + dx
             dx *= 2
             dy *= 2
-            d = (2 * dy) + dx
-            while x < x1:
+
+            while (x < x1):
                 plot(screen, color, x, y)
                 if (d > 0):
-                    y -= 1
+                    y += 1
                     d += dx
                 x += 1
-                d -= dy
+                d += dy
+        #second octant
         else:
-            #octant 2
+            d = dy + (2 * dx)
             dx *= 2
             dy *= 2
-            d = dy + (2 * dx)
-            while y < y1:
+
+            while (y < y1):
                 plot(screen, color, x, y)
                 if (d < 0):
                     x += 1
                     d += dy
                 y += 1
                 d += dx
+    #4th quadrant
     else:
-        #quadrant 4
-        if dx <= dy:
-            #octant 8
+        #eighth octant
+        if (dx <= dy):
+            d = (2 * dy) - dx
             dx *= 2
             dy *= 2
-            d = (2 * dy) - dx
-            while x < x1:
+
+            while (x < x1):
                 plot(screen, color, x, y)
                 if (d < 0):
                     y -= 1
                     d -= dx
                 x += 1
                 d += dy
+        #seventh octant
         else:
-            #octant 7
+            d = dy - (2 * dx)
             dx *= 2
             dy *= 2
-            d = dy - (2 * dx)
-            while y > y1:
+
+            while (y > y1):
                 plot(screen, color, x, y)
                 if (d > 0):
                     x += 1
                     d += dy
                 y -= 1
                 d -= dx
-            
-                
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import random
 from display import *
 from draw import *
 
 screen = new_screen()
 color = [ 255, 255, 255 ]
 
+#all the randoms
+#draws 204 lines because i finished this at 2:04
+for i in range(204):
+    draw_line(random.randint(0, 500), random.randint(0, 500), random.randint(0, 500), random.randint(0, 500), screen, [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255) ])
 
-draw_line(250, 250, 300, 200, screen, color)
 display(screen)
 save_extension(screen, 'img.png')
